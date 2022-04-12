2fe8d771f64905da4d017be35ec399308a0fb60b
gui-ying233 <82418456+gui-ying233@users.noreply.github.com>
2022-04-12 10:29:10 -0400

    统一配置

--- 作死小帮手.js
+++ 作死小帮手.js
@@ -1,9 +1,6 @@
 const { MediaWikiJS } = require('@lavgup/mediawiki.js')
-const bot = new MediaWikiJS({
-    url: 'https://mzh.moegirl.org.cn/api.php',
-    botUsername: '机娘鬼影233号@速记羽毛笔',
-    botPassword: 'sk8adp6m2h8gp4c7nfktdihrmqpo9kde',
-})
+const config = require('./config.js')
+const bot = new MediaWikiJS(config.zh)
 bot
     .login()
     .then(async () => {
