d53b6da4c86c55ca6e45ba7d7f7eda884332bd4f
鬼影233 <82418456+gui-ying233@users.noreply.github.com>
2022-06-01 17:09:28 -0400

    -

--- 清理者.js
+++ 清理者.js
@@ -13,7 +13,6 @@ async function cleaner(gcmtitle, regex, replace = '', skipTitle = /^$/) {
 			gcmnamespace: '-2|-1|0|1|4|5|6|7|8|9|10|11|12|13|14|15|274|275|710|711|828|829|2300|2302|2303',
 			gcmlimit: 'max',
 			gcmtitle,
-			gcmendsortkeyprefix,
 		})
 		if (result1.query === undefined) {
 			console.log('无页面');
