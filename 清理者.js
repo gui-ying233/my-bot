4b31963d6d07b6970ed880184e9bae62a990e3df
鬼影233 <82418456+gui-ying233@users.noreply.github.com>
2022-06-01 15:58:50 -0400

    重写页面跳过判断
    暂无测试环境故提交个分支。

--- 清理者.js
+++ 清理者.js
@@ -3,7 +3,7 @@
 const { MediaWikiJS } = require('@lavgup/mediawiki.js')
 const { CronJob } = require( 'cron');
 const bot = new MediaWikiJS(require('./config.json').mzh)
-async function cleaner(gcmtitle, regex, replace, gcmendsortkeyprefix) {
+async function cleaner(gcmtitle, regex, replace = '', skipTitle = /^$/) {
 	try {
 		const result1 = await bot.api.get({
 			action: 'query',
@@ -21,7 +21,9 @@ async function cleaner(gcmtitle, regex, replace, gcmendsortkeyprefix) {
 			console.log(`${gcmtitle}中共${result1.query.pages.length}个页面。`)
 			for (let i = 0; i < result1.query.pages.length; i++) {
 				console.log(`第${i+1}个页面：${result1.query.pages[i].title}`);
-				if (result1.query.pages[i].revisions[0].content.match(/{{:?(?:Template:|[模样樣]板:|T:)?(?:施工中|[编編][辑輯]中|inuse)/gi) !== null) {
+				if(! new RegExp(/^(?:Template:Sandbox|Template:沙盒|模块:Sandbox)\//).test(result1.query.pages[i].title) && ! new RegExp(skipTitle).test(result1.query.pages[i].title)) {
+					console.log("跳过页面");
+				} else if (result1.query.pages[i].revisions[0].content.match(/{{:?(?:Template:|[模样樣]板:|T:)?(?:施工中|[编編][辑輯]中|inuse)/gi) !== null) {
 					console.log("施工中");
 				} else {
 					try {
@@ -51,11 +53,11 @@ const cronJob=new CronJob({
 		onTick: async () => {
 			var d = new Date()
 			console.log(`${d.getFullYear()}-${String(d.getMonth()+1).padStart(2,0)}-${String(d.getDate()).padStart(2,0)} ${String(d.getHours()).padStart(2,0)}:${String(d.getMinutes()).padStart(2,0)}:${String(d.getSeconds()).padStart(2,0)}`);
-			await cleaner('CAT:错误使用标题格式化的页面', /{{:?(?:Template:|[模样樣]板:|T:)?[标標][题題]格式化}}\n?/gi, '');
+			await cleaner('CAT:错误使用标题格式化的页面', /{{:?(?:Template:|[模样樣]板:|T:)?[标標][题題]格式化}}\n?/gi);
 			await cleaner('CAT:需要更换为标题格式化的页面', /{{:?(?:Template:|[模样樣]板:|T:)?[标標][题題]替[换換].*?}}/gis, '{{标题格式化}}');
 			await cleaner('CAT:需要更换为小写标题的页面', /{{:?(?:Template:|[模样樣]板:|T:)?[标標][题題]替[换換].*?}}/gis, '{{小写标题}}');
-			await cleaner('CAT:不必要使用override参数的音乐条目', /\|override=1\n?/g, '');
-			await cleaner('CAT:错误使用标题替换模板的页面', /{{:?(?:Template:|[模样樣]板:|T:)?[标標][题題]替[换換].*?}}\n?/gis, '', 'CAT:需要更换为');
+			await cleaner('CAT:不必要使用override参数的音乐条目', /\|override=1\n?/g);
+			await cleaner('CAT:错误使用标题替换模板的页面', /{{:?(?:Template:|[模样樣]板:|T:)?[标標][题題]替[换換].*?}}\n?/gis, skipTitle = /^CAT:需要更换为(?:标题格式化|小写标题)的页面$/);
 		}
 });
 bot.login().then(() => cronJob.start());
