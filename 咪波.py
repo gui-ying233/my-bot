42fc2d146afcbf663ba444597857ecb051eeac1c
MC_鬼影 <zhuyufei20050307@gmail.com>
2022-08-15 15:22:37 -0400

    增加自动更名

--- 咪波.py
+++ 咪波.py
@@ -4,7 +4,7 @@ urlTitleList = []
 urlList = []
 title = ''
 
-titles = 'File:头像 敌人 酒类爱好者.png|File:头像 敌人 酒类协会会员.png|File:头像 敌人 湖畔志愿者.png|File:头像 敌人 湖畔救生员.png|File:头像 敌人 沙堡建筑者.png|File:头像 敌人 沙堡设计师.png|File:头像 敌人 排球发球车.png|File:头像 敌人 排球重扣车.png|File:头像 敌人 自走车.png|File:头像 敌人 荧壳源石虫.png|File:头像 敌人 钢铁冲浪教练.png|File:头像 敌人 合金冲浪教练.png|File:头像 敌人 全封闭沙滩车.png|File:头像 敌人 竞赛级沙滩车.png|File:头像 敌人 蜜酿级醒酒助手.png|File:头像 敌人 烈酒级醒酒助手.png|File:头像 敌人 “物欲拮抗者”.png|File:头像 敌人 “漫游小屋”.png'
+titles = 'File:头像 敌人 自走车.png'
 
 while 1:
     try:
@@ -33,9 +33,11 @@ for _ in range(len(result)):
         elif title[:3] == '半身像' or title[:2] == '立绘' or title[:2] == '地图' or title[:2] == '技能' or title[:5] == '道具 带框' or title[:2] == '模组' or title[:2] == '图标':
             title = '明日方舟' + title
         elif title[:2] == '道具' or title[:2] == '主题' and title[-6:-4] == '总览':
-            title = '明日方舟', title
+            title = '明日方舟 ' + title
         elif title[:2] == '头像':
             title = '明日方舟 tx' + title[2:]
+        elif title[:5] == '情报处理室':
+            title = '明日方舟剧情 ' + title[6:]
         try:
             print(f'【{_ + 1}/{len(result)}】\t{title}\t下载中...')
             open('/Users/emmm/Desktop/明日方舟/图片/'+title, "wb").write(
