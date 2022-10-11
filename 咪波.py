34c688c32b7ccd35ce0bbfe9afb5b0407bfd5e63
MC_鬼影 <zhuyufei20050307@gmail.com>
2022-10-11 18:48:26 -0400

    feat: :sparkles: 改了许多，主要是增加新功能
    增加新功能例如预设新干员与新皮肤的图片获取

--- 咪波.py
+++ 咪波.py
@@ -1,19 +1,48 @@
-import requests
 from hashlib import sha1
+
+import requests
 
 urlTitleList = []
 urlList = []
+titles = []
 title = ''
 
-titles = ''
 
+def 新干员(new: str) -> list[str]:
+    global titles
+    for _ in new.split('|'):
+        titles += ['头像_'+_, '头像_'+_+'_2', '半身像_'+_+'_1',
+                   '半身像_'+_+'_2', '立绘_'+_+'_1', '立绘_'+_+'_2']
+    return titles
+
+
+def 新时装(new: str, id: int = 1) -> list[str]:
+    global titles
+    id = str(id)
+    for _ in new.split('|'):
+        titles += ['头像_'+_+'_skin'+id, '半身像_' +
+                   _+'_skin'+id, '立绘_'+_+'_skin'+id+'']
+    return titles
+
+
+新时装('爱丽丝|老鲤|风丸')
+
+新干员('白铁|达格达|明椒|铅踝')
+
+titles += '头像_敌人_特雷西斯.png'.split('|')
+
+for _ in range(len(titles)):
+    if titles[_][:3] != '文件:' and titles[_][:5] != 'File:':
+        titles[_] = '文件:' + titles[_]
+    if titles[_][-4:] != '.png':
+        titles[_] += '.png'
 while 1:
     try:
         result0 = requests.get('https://m.prts.wiki/api.php',
                                params={
                                    'action': 'query',
                                    'prop': 'imageinfo',
-                                   'titles': titles,
+                                   'titles': '|'.join(titles),
                                    'iiprop': 'url|sha1',
                                    'format': 'json',
                                    'formatversion': 'latest'
@@ -28,7 +57,7 @@ for _ in range(len(result)):
     try:
         if result[_]['missing']:
             print(f'【{_ + 1}/{len(result)}】\t{title}\t未找到文件')
-    except Exception as e:
+    except:
         if title[:4] == '模组类型':
             title = 'AkModuleType' + title[4:]
         elif title[:3] == '半身像' or title[:2] == '立绘' or title[:2] == '地图' or title[:2] == '技能' or title[:5] == '道具 带框' or title[:2] == '模组' or title[:2] == '图标':
@@ -41,9 +70,13 @@ for _ in range(len(result)):
             title = '明日方舟剧情 ' + title[6:]
         elif title[:6] == '职业分支图标':
             title = '明日方舟职业_分支' + title[6:]
+        elif title[:9] == 'Skin logo':
+            title = 'Skinlogo' + title[9:]
+        elif title[:3] == '主题图':
+            title = title[4:]
         try:
             while 1:
-                print(f'【{_ + 1}/{len(result)}】\t{title}\t下载中...')
+                print(f'【{_ + 1}/{len(result)}】\t{title}\t下载中…')
                 open('/Users/emmm/Desktop/明日方舟/图片/'+title, "wb").write(
                     requests.get(result[_]['imageinfo'][0]['url']).content)
                 if result[_]['imageinfo'][0]['sha1'] == sha1(open('/Users/emmm/Desktop/明日方舟/图片/'+title, "rb").read()).hexdigest():
