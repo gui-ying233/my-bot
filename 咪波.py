d07050854dba281d8bbd1db22027765ffe9c6e92
鬼影233 <82418456+gui-ying233@users.noreply.github.com>
2022-12-16 15:15:33 -0500

    + 新关卡自动查找需求图片

--- 咪波.py
+++ 咪波.py
@@ -10,8 +10,10 @@ titles = []
 def 新干员(new: str) -> list[str]:
     global titles
     for _ in new.split('|'):
-        titles += ['头像_'+_, '头像_'+_+'_2', '半身像_'+_+'_1',
-                   '半身像_'+_+'_2', '立绘_'+_+'_1', '立绘_'+_+'_2', '道具_带框_'+_+'的信物']
+        titles += [
+         '头像_' + _, '头像_' + _ + '_2', '半身像_' + _ + '_1', '半身像_' + _ + '_2',
+         '立绘_' + _ + '_1', '立绘_' + _ + '_2', '道具_带框_' + _ + '的信物'
+        ]
     return titles
 
 
@@ -19,15 +21,50 @@ def 新时装(new: str, id: int = 1) -> list[str]:
     global titles
     id = str(id)
     for _ in new.split('|'):
-        titles += ['头像_'+_+'_skin'+id, '半身像_' +
-                   _+'_skin'+id, '立绘_'+_+'_skin'+id+'']
+        titles += [
+         '头像_' + _ + '_skin' + id, '半身像_' + _ + '_skin' + id,
+         '立绘_' + _ + '_skin' + id + ''
+        ]
     return titles
 
 
 def 新技能(new: str) -> list[str]:
     global titles
     for _ in new.split('|'):
-        titles.append('技能_'+_)
+        titles.append('技能_' + _)
+    return titles
+
+
+def 新关卡(new: str) -> list[str]:
+    global titles
+    while 1:
+        try:
+            result1 = requests.get('https://mzh.moegirl.org.cn/api.php',
+                                   params={
+                                    "action": "query",
+                                    "format": "json",
+                                    "titles": new,
+                                    "generator": "images",
+                                    "utf8": 1,
+                                    "formatversion": 2,
+                                    "gimlimit": "max"
+                                   })
+            break
+        except Exception as e:
+            print(str(e), "重试...")
+    result = result1.json()['query']['pages']
+    for _ in result:
+        if _["missing"]:
+            try:
+                if _["known"]:
+                    pass
+            except:
+                if _["title"][10:12] == "tx" and _["title"][13:15] == "敌人":
+                    titles.append("File:头像 敌人" + _["title"][15:])
+                elif _["title"][-6:-4] == "地图":
+                    titles.append(_["title"])
+                else:
+                    print("非预期图片：" + _["title"])
     return titles
 
 
@@ -48,12 +85,12 @@ def main() -> None:
         try:
             result0 = requests.get('https://m.prts.wiki/api.php',
                                    params={
-                                       'action': 'query',
-                                       'prop': 'imageinfo',
-                                       'titles': '|'.join(titles),
-                                       'iiprop': 'url|sha1',
-                                       'format': 'json',
-                                       'formatversion': 'latest'
+                                    'action': 'query',
+                                    'prop': 'imageinfo',
+                                    'titles': '|'.join(titles),
+                                    'iiprop': 'url|sha1',
+                                    'format': 'json',
+                                    'formatversion': 'latest'
                                    })
             break
         except Exception as e:
@@ -68,7 +105,14 @@ def main() -> None:
         except:
             if title[:4] == '模组类型':
                 title = 'AkModuleType' + title[4:]
-            elif title[:3] == '半身像' or title[:2] == '立绘' or title[:2] == '地图' or title[:2] == '技能' or title[:5] == '道具 带框' or title[:2] == '模组' or title[:2] == '图标':
+            elif title[:
+                       3] == '半身像' or title[:
+                                            2] == '立绘' or title[:
+                                                                2] == '地图' or title[:
+                                                                                    2] == '技能' or title[:
+                                                                                                        5] == '道具 带框' or title[:
+                                                                                                                               2] == '模组' or title[:
+                                                                                                                                                   2] == '图标':
                 title = '明日方舟' + title
             elif title[:2] == '道具' or title[:2] == '主题' and title[-6:-4] == '总览':
                 title = '明日方舟 ' + title
@@ -85,9 +129,10 @@ def main() -> None:
             try:
                 while 1:
                     print(f'【{_ + 1}/{len(result)}】\t{title}\t下载中…')
-                    open('./图片/'+title, "wb").write(
-                        requests.get(result[_]['imageinfo'][0]['url']).content)
-                    if result[_]['imageinfo'][0]['sha1'] == sha1(open('./图片/'+title, "rb").read()).hexdigest():
+                    open('./图片/' + title,
+                         "wb").write(requests.get(result[_]['imageinfo'][0]['url']).content)
+                    if result[_]['imageinfo'][0]['sha1'] == sha1(
+                      open('./图片/' + title, "rb").read()).hexdigest():
                         print('\t下载完成')
                         break
                     else:
@@ -97,9 +142,10 @@ def main() -> None:
     print('进程结束')
 
 
-清除()
+# 清除()
 # 新时装('')
 # 新干员('')
 # 新技能('')
+新关卡("明日方舟/照我以火/活动关卡")
 # titles += ''.split('|')
 main()
